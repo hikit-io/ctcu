70a75e1d7f4569dca35e76e52fc993fd3c89aa6e
Nekilc <nieaowei@gmail.com>
2022-12-12 19:24:58 +0800

    feat: copy to json

--- Ctcu Extension/SafariExtensionHandler.swift
+++ Ctcu Extension/SafariExtensionHandler.swift
@@ -7,6 +7,11 @@
 
 import SafariServices
 
+struct Page:Codable{
+    var title:String
+    var link:String
+}
+
 class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
@@ -19,6 +24,35 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func toolbarItemClicked(in window: SFSafariWindow) {
         // This method will be called when your toolbar item is clicked.
         NSLog("The extension's toolbar item was clicked")
+        window.getActiveTab { tab in
+            tab?.getActivePage(completionHandler: { page in
+                page?.getPropertiesWithCompletionHandler({ props in
+                    
+                    
+                    let page = Page(title: props?.title ?? "", link: props?.url?.absoluteString ?? "")
+                    
+                    NSLog(page.link)
+                    
+                    
+                    let jsonencode = JSONEncoder()
+                    if #available(macOSApplicationExtension 10.15, *) {
+                        jsonencode.outputFormatting = .withoutEscapingSlashes.union(.prettyPrinted)
+                    } else {
+//                        jsonencode.outputFormatting = .prettyPrinted
+                    }
+                    
+                    let jsonData  = try? jsonencode.encode(page)
+                    let jsonString = String(data: jsonData!, encoding: .utf8)!
+
+                    
+                    NSLog(jsonString)
+                    
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString(jsonString, forType: NSPasteboard.PasteboardType.string)
+                    
+                })
+            })
+        }
     }
     
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
