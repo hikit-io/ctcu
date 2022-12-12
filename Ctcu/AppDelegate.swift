117a7bc80f5127060b3f96b34189002292ff6dc1
Nekilc <nieaowei@gmail.com>
2022-12-12 18:53:52 +0800

    Initial Commit

---
+++ Ctcu/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Ctcu
+//
+//  Created by Nekilc on 2022/12/12.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
